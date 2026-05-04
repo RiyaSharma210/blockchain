093471727feaa3aec9fd595151b61b1b90dd7318
RiyaSharma210 <rsharma27092006@gmail.com>
2026-05-04 14:52:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,52 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from blockchain import Blockchain
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
+blockchain = Blockchain()
+
+@app.route('/')
 def home():
-    return "Blockchain App is Running 🚀"
+    return render_template('index.html')
+
+@app.route('/add_product', methods=['POST'])
+def add_product():
+
+    product_name = request.form['product_name']
+    manufacturer = request.form['manufacturer']
+
+    previous_block = blockchain.get_previous_block()
+
+    blockchain.create_block(
+        {
+            'product_name': product_name,
+            'manufacturer': manufacturer
+        },
+        previous_block['hash']
+    )
+
+    return render_template(
+        'success.html',
+        product_name=product_name
+    )
+
+@app.route('/verify')
+def verify_page():
+    return render_template('verify.html')
+
+@app.route('/verify_product', methods=['POST'])
+def verify_product():
+
+    product_name = request.form['product_name']
+
+    result = blockchain.verify_product(product_name)
+
+    return render_template(
+        'result.html',
+        result=result,
+        product_name=product_name
+    )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
